c9f0ac9c8f57ad59cffda6d0d9a5d55908cc40dd
Ayush Tripathi <ayushtripathi766@gmail.com>
2022-04-01 14:26:21 +0530

    Updated Elemoney

--- src/pages/ELEMoney.js
+++ src/pages/ELEMoney.js
@@ -61,95 +61,41 @@ const titleStyle = { fontFamily:"Montserrat", backgroundColor: "#045B62", color:
 
 const data = [
   {
-    sno: 1,
-    purpose: "Course Waiver to Ph.D. Scholar",
-    code: "PH1",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph1.pdf",
-    name: "Convener (DPGC)",
+    month: "Jan",
+    unit: "",
+    cylinder: "",
+    total: "",
+    Datum: "446.894",
+    percent: "",
+    carbocreds: ""
   },
   {
-    sno: 2,
-    purpose: "Constitution of Doctoral Scrutiny Committee (DSC)",
-    code: "PH2",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph2.pdf",
-    name: "Dean (Academic)",
+    month: "Jan",
+    unit: "",
+    cylinder: "",
+    total: "",
+    Datum: "446.894",
+    percent: "",
+    carbocreds: ""
   },
   {
-    sno: 3,
-    purpose:
-      "Form for 2nd Semester Course Work Proposed By DSC for Ph.D Scholar",
-    code: "PH3",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph3.pdf",
-    name: "Dean (Academic)",
-  },
-  {
-    sno: 4,
-    purpose: "Comprehensive Examination Report",
-    code: "PH4",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph4.pdf",
-    name: "Dean (Academic)",
-  },
-  {
-    sno: 5,
-    purpose: "Research Proposal Seminar Report",
-    code: "PH5",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph5.pdf",
-    name: "Dean (Academic)",
-  },
-  {
-    sno: 6,
-    purpose: "Pre-Submission Thesis Assessment by Doctoral Scrutiny Committee",
-    code: "PH6",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph6.pdf",
-    name: "Convener (DPGC)",
-  },
-  {
-    sno: 7,
-    purpose: "Pre-Submission Report of Ph.D. Scholar",
-    code: "PH7",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph7.pdf",
-    name: "Convener (DPGC)",
-  },
-  {
-    sno: 8,
-    purpose: "Certificate for Acceptance of Draft Thesis by DSC ",
-    code: "PH7-A",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph7a.pdf",
-    name: "Convener (DPGC)",
-  },
-  {
-    sno: 9,
-    purpose: "Suggested Panel of Examiners",
-    code: "PH8",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph8.pdf",
-    name: "Dean (Academic)",
-  },
-  {
-    sno: 10,
-    purpose: "Particulars of candidate for Submission of Synopsis for Ph.D",
-    code: "PH9",
-    url: "https://www.iitism.ac.in/~academics/assets/acad_forms/ph9.pdf",
-    name: " Dean (Academic)",
-  },
+    month: "Jan",
+    unit: "",
+    cylinder: "",
+    total: "",
+    Datum: "446.894",
+    percent: "",
+    carbocreds: ""
+  }
 ];
 const columns = [
-  { title: "Datum Emmission", field: "sno" },
-  { title: "Current Emmision", field: "purpose" },
-  { title: "% Reduced", field: "code" },
-  // {
-  //   title: "Download",
-  //   field: "url",
-  //   render: (rowData) => (
-  //     <a href={rowData.url}>
-  //       <img
-  //         alt=""
-  //         class="icon"
-  //         src="https://img.icons8.com/fluent/48/000000/pdf.png"
-  //       />
-  //     </a>
-  //   ),
-  // },
-  { title: "Carbocredits Balance", field: "name" },
+  { title: "Month", field: "month"},
+  { title: "Electricity (Units)", field: "unit" },
+  { title: "Gas (Cylinders)", field: "cylinder" },
+  { title: "Total Emmission (kg)", field: "total" },
+  { title: "Datum Emission (kg)", field: "datum" },
+  { title: "% Reduced", field: "percent" },
+  { title: "CraboCredits Earned", field:"carbocreds"}
 ];
 
 function ELEMoney() {
